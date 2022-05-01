b8cb75ad14c56104d3ccdacb07803ff2ffa65359
sonke <inwoker123@mail.ru>
2022-05-02 00:04:36 +0300

    Changes in the test

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -14,12 +14,13 @@ public class StatisticsServiceTest {
         long actual = service.findMax(incomesInBillions);
         assertEquals(expected, actual);
     }
-//    @Test
-//    void findMax() {
-//        StatisticsService service = new StatisticsService();
-//        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-//        long expected = 12;
-//        long actual = service.findMax(incomesInBillions);
-//        assertEquals(expected, actual);
-//    }
+
+    @Test
+    void findMaxWithOtherParams() {
+        StatisticsService service = new StatisticsService();
+        long[] incomesInBillions = {4, 7, 10, 11, 20, 3, 16, 40, 12, 20};
+        long expected = 40;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
 }
